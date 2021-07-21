91912c0369e5b4562d480e1aafbce4ba34f6568e
ionmoles <iainmoyles@gmail.com>
2021-07-20 21:47:48 -0400

    Updated read_data
    Updated to also read vaccine data

--- read_data.m
+++ read_data.m
@@ -3,7 +3,7 @@ clear variables
 
 year = '2021';
 mday = 'Jul20';
-data_flag = 0;
+data_flag = 1;
 % 0 - covid cases
 % 1 - vaccine data
 
@@ -29,9 +29,19 @@ switch data_flag
         DATA_T = linspace(0,Tlen-1,Tlen)';
         DATA_pos = T{date_spot:end,5};
         DATA_tot = T{date_spot:end,8};
+    case 1
+        DATA_notes.time={'first vaccine data Jan 7, 2021','first point is 302 days from March 10'};
+        start_date='2021-01-06';
+        date_spot = find(T{:,1}==start_date);
+        Tlen = size(T,1)-date_spot+1;
+        DATA_T = linspace(302,302+Tlen-1,Tlen)';
+        DATA_tot1 = T{date_spot:end,6};
+        DATA_totfull = T{date_spot:end,8};
 end
 
 switch data_flag
     case 0
         save(['DATA-',year,'-',mday,'.mat'],'DATA_notes','DATA_T','DATA_pos','DATA_tot');
+    case 1
+        save(['vDATA-',year,'-',mday,'.mat'],'DATA_notes','DATA_T','DATA_tot1','DATA_totfull');
 end
