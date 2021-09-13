a0fb6a73542380a91260ca867c6c8f5eda7f3cb6
ionmoles <iainmoyles@gmail.com>
2021-09-13 17:10:42 -0400

    Updated to read variants
    read_data now reads variants and stores them with 7 day averages
    generate_media_vaccine will generate the plots for any files for second publication/presentation

---
+++ generate_media_vaccine.m
@@ -0,0 +1,95 @@
+close all
+clear variables
+
+date_flag = 1;
+
+loop_flag = 0;
+
+%% BASIC SETUP-DO NOT NEED TO EDIT GENERALLY
+if date_flag
+    base_date = datenum('10-Mar-2020','dd-mmm-yy');
+else
+    base_date = 0;
+end
+
+pcols = {1/255*[0,196,255],1/255*[225,12,62],1/255*[55,176,116],1/255*[244,201,107],1/255*[149,52,235],1/255*[176,176,176]};
+symbs = {'o','x','.','+','s'};
+
+curpath = pwd;
+newpath = strcat(curpath(1:length(curpath)-6),'\data_figs\');
+plotpath = strcat(curpath(1:length(curpath)-6),'\plots\vac_plots\');
+runpath = strcat(curpath,'\runs\');
+
+pos = [2,4.5,6,4.5];
+
+exp_len = 1;
+sub_len = [0];
+%% 
+
+%0 - plot variants
+
+done = 0;
+experiment = 0;
+sub_experiment = 0;
+
+if loop_flag
+    experiment =0;
+    sub_experiment =0;
+end
+
+while ~done
+    close all
+    clear fig
+    clr_flag = 0;
+    if ~loop_flag
+        done = 1;
+    else
+        fprintf('Currently running experiment %i sub experiment %i...\n',experiment,sub_experiment);
+    end
+
+    switch experiment
+        case 0
+            DATA = load('variant_DATA-2021-Sept13.mat');
+            [fig,figprop]=new_figure('variants_plot',pos);
+            
+            hold on
+            for k=1:DATA.DATA_notes.varsize
+                plot(base_date+DATA.DATA_T(7:end),DATA.var_avg(:,k)./DATA.new_avg,'Marker',symbs{k},'LineStyle','none')
+            end
+            hold off
+            xlim([base_date+DATA.DATA_notes.time_var,base_date+length(DATA.DATA_T)]);
+            if date_flag
+                datetick('x','dd-mmm-yy','keepticks','keeplimits');
+                xtickangle(45);
+            else
+                figprop.xlab=xlabel('$t$ (days since March 10, 2020)');
+            end
+            figprop.ylab=ylabel('Percent of cases (7-day average)');
+            figprop.ax = gca;
+            figprop.leg = legend({'$\alpha$','$\beta$','$\gamma$','$\delta$'},'location','northwest');
+            figprop.fnt_size = 14;
+            
+            
+    end
+    
+    if loop_flag
+        if sub_len(experiment+1)>0
+            sub_experiment = sub_experiment +1;
+                if sub_experiment>sub_len(experiment+1)
+                    sub_experiment = 0;
+                    experiment = experiment +1;
+                    clr_flag = 1;
+                end
+        else
+            experiment = experiment +1;
+            clr_flag = 1;
+        end
+        if clr_flag
+            clearvars -except date_flag base_date loop_flag pcols curpath newpath plotpath runpath pos exp_len sub_len done experiment sub_experiment fig figprop
+        end
+        if experiment > exp_len
+            done =1;
+        end
+    end
+    figure_revise(pos,plotpath,fig,figprop);
+end
--- read_data.m
+++ read_data.m
@@ -2,13 +2,14 @@ close all
 clear variables
 
 year = '2021';
-mday = 'Jul20';
-data_flag = 1;
+mday = 'Sept13';
+data_flag = 2;
 % 0 - covid cases
 % 1 - vaccine data
+% 2 - variant cases
 
 switch data_flag
-    case 0
+    case {0,2}
         basetitle='covidtesting';
     case 1
         basetitle='vaccine_doses';
@@ -37,6 +38,24 @@ switch data_flag
         DATA_T = linspace(302,302+Tlen-1,Tlen)';
         DATA_tot1 = T{date_spot:end,6};
         DATA_totfull = T{date_spot:end,8};
+    case 2
+        T = fillmissing(T,'previous');
+        DATA_notes.time={'variants first recorded Jan 29, 2021','first time point Mar 10, 2020','variants first measured 326 days after March 10'};
+        DATA_notes.time_var = 326; %corresponds to variants starting 326 days after Mar 10
+        DATA_notes.varsize=4;
+        start_date='2020-03-10';
+        date_spot = find(T{:,1}==start_date);
+        variants_nan = isnan(T{:,24:24+DATA_notes.varsize-1});
+        for k=1:DATA_notes.varsize
+            T{variants_nan(:,k),24+(k-1)}=0;
+        end
+        Tlen = size(T,1)-date_spot+1;
+        DATA_T = linspace(0,Tlen-1,Tlen)';
+        DATA_tot = T{date_spot:end,8};
+        DATA_new = T{date_spot:end,8}-T{date_spot-1:end-1,8};
+        DATA_var = T{date_spot:end,24:24+DATA_notes.varsize-1}-T{date_spot-1:end-1,24:24+DATA_notes.varsize-1};
+        new_avg = (DATA_new(1:end-6)+DATA_new(2:end-5)+DATA_new(3:end-4)+DATA_new(4:end-3)+DATA_new(5:end-2)+DATA_new(6:end-1)+DATA_new(7:end))/7;
+        var_avg = (DATA_var(1:end-6,:)+DATA_var(2:end-5,:)+DATA_var(3:end-4,:)+DATA_var(4:end-3,:)+DATA_var(5:end-2,:)+DATA_var(6:end-1,:)+DATA_var(7:end,:))/7;
 end
 
 switch data_flag
@@ -44,4 +63,6 @@ switch data_flag
         save(['DATA-',year,'-',mday,'.mat'],'DATA_notes','DATA_T','DATA_pos','DATA_tot');
     case 1
         save(['vDATA-',year,'-',mday,'.mat'],'DATA_notes','DATA_T','DATA_tot1','DATA_totfull');
+    case 2
+        save(['variant_DATA-',year,'-',mday,'.mat'],'DATA_notes','DATA_T','DATA_tot','DATA_new','DATA_var','new_avg','var_avg');
 end
